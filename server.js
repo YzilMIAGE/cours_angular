6de61a4cc76e34ba5a4ddafcef947eed66d44707
ADR-Clizy <adr.clizy@outlook.fr>
2022-11-10 09:59:48 +0100

    fixing server

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require("path");
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + "/dist/assignment-app/"));
+app.use(express.static(__dirname + "/dist/assignments/"));
 
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/assignment-app/index.html"));
+  res.sendFile(path.join(__dirname + "/dist/assignments/index.html"));
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8081);
